aaa2a11bbccc8e5e943627b392fedf830b206d27
Mateusz Steliga <steelman22@gmail.com>
2022-02-21 19:17:32 +0100

    removed unused scripts

--- celery_startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-celery -A eMenu worker --loglevel=Info &
-celery -A eMenu beat --loglevel=Info &
-wait -n
-exit $?
-
--- web_startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#celery -A eMenu worker --loglevel=Info &
-#celery -A eMenu beat --loglevel=Info &
-python manage.py runserver "0.0.0.0:8000"
-
